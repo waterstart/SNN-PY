8a391ea8d93f33cddcbe62897c97d8079ffce097
Abdulrahmon tijani <schoolrumblefan2008@gmail.com>
2025-07-23 10:17:08 +1000

    made simple neural model

--- DataTools.py
+++ DataTools.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import os
 from pathlib import Path
 import numpy
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,17 @@
+import torch
+import torch.nn as NeuralNetwork
+import torch.nn.functional as Functional
 
-def TrainNeurons(TrainingTensor):
-    print(TrainingTensor)
+class Model(NeuralNetwork.Module):
+    def __init__(self, NumNodes, HidLayer1 = 8,HidLayer2 = 9, OutLayer=5):
+        super().__init__()
+        self.BridgeNto1 = NeuralNetwork.Linear(NumNodes,HidLayer1)
+        self.BridgeH1toH2 = NeuralNetwork.Linear(HidLayer1,HidLayer2)
+        self.BridgeH2toOut = NeuralNetwork.Linear(HidLayer2,OutLayer)
+    
+    def Forward(self,Node):
+        Node = Functional.relu(self.BridgeNto1)
+        Node = Functional.relu(self.BridgeH1toH2)
+        Node = self.BridgeH2toOut(Node)
+
     
