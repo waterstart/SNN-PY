26b34225cdf54b45c8767b142ffeee13839a462b
Abdulrahmon tijani <schoolrumblefan2008@gmail.com>
2025-07-26 00:06:20 +1000

    added all the stuff imade tday

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,7 +4,7 @@ import torch.nn.functional as Functional
 from sklearn.model_selection import train_test_split
 import torch.optim.adam 
 import matplotlib.pyplot as plt
-
+import numpy as np
 class AutoEncoder(NeuralNetwork.Module):
     def __init__(self,NumNodes, LatentDimensions = 32):
         super().__init__()
@@ -33,7 +33,7 @@ class AutoEncoder(NeuralNetwork.Module):
 
 
 class Model(NeuralNetwork.Module):
-    def __init__(self, NumNodes, HidLayer1 = 128,HidLayer2 = 64,HidLayer3 = 32, OutLayer=5):
+    def __init__(self, NumNodes, HidLayer1 = 64,HidLayer2 = 32,HidLayer3 = 32, OutLayer=5):
         super().__init__()
         self.BridgeNto1 = NeuralNetwork.Linear(NumNodes,HidLayer1)
         self.BridgeH1toH2 = NeuralNetwork.Linear(HidLayer1,HidLayer2)
@@ -50,15 +50,16 @@ class Model(NeuralNetwork.Module):
     
     def ReadAndTrain(self, FeatureSet,AnswerSet):
            ModelCriterion = NeuralNetwork.MSELoss()
-           ModelOptimiser = torch.optim.Adam(self.parameters(), lr=0.1, weight_decay=1e-4)
-           EpochAmount = 10e+5
+           ModelOptimiser = torch.optim.Adam(self.parameters(), lr=0.001 ,weight_decay=1e-4)
+           EpochAmount = 100000
            Losses = []
    
 
 
            for FeatureList, AnswerList in zip(FeatureSet, AnswerSet):
 
-            FeatureTrain, FeatureTest, AnswerTrain, AnswerTest = train_test_split(FeatureList.to_numpy(),AnswerList.to_numpy(),test_size=0.1)
+            FeatureTrain, FeatureTest, AnswerTrain, AnswerTest = train_test_split(FeatureList.detach().cpu().numpy(),
+    AnswerList.detach().cpu().numpy(),test_size=0.2)
            
 
             FeatureTrain =  torch.FloatTensor(FeatureTrain)
@@ -68,13 +69,13 @@ class Model(NeuralNetwork.Module):
             AnswerTest = torch.FloatTensor(AnswerTest)
 
             for index in range(EpochAmount):
-                AnswerPredict = torch.FloatTensor(self.Forward(FeatureTrain))
+                AnswerPredict = self.forward(FeatureTrain)
                 Loss = ModelCriterion(AnswerPredict,AnswerTrain )
 
                 Losses.append(Loss.detach().numpy())
 
                 if index % 10000 == 0:
-                        print(f'Epoch: {index} and loss: {Loss}')
+                        print(f'Epoch: {index} and loss: {Loss} and sqrt loss {np.sqrt(Loss.detach().numpy())}')
                 
                 ModelOptimiser.zero_grad()
                 Loss.backward()
@@ -94,17 +95,21 @@ class TreeModel():
 
            for FeatureList, AnswerList in zip(FeatureSet, AnswerSet):
 
-            FeatureTrain, FeatureTest, AnswerTrain, AnswerTest = train_test_split(FeatureList,AnswerList,random_state=17,test_size=0.2)
+            FeatureTrain, FeatureTest, AnswerTrain, AnswerTest = train_test_split( FeatureList.detach().cpu().numpy(),
+            AnswerList.detach().cpu().numpy(),random_state=17,test_size=0.2)
             print("feature list: ",type(FeatureList))
             print("Answer list: ", type(AnswerList))
 
-            RFC =RandomForestRegressor(n_estimators=500)
+            RFC =RandomForestRegressor()
             RFC.fit(FeatureTrain,AnswerTrain)
             AnswerPred = RFC.predict(FeatureTest)
             RFC.score(FeatureTest,AnswerTest)
 
             MeanScoreError = mean_squared_error(AnswerTest, AnswerPred)
+            SQRMeanScoreError = np.sqrt(mean_squared_error(AnswerTest, AnswerPred))
+
             print("error squared bro please: ",MeanScoreError)
+            print("error reqsquared bro please: ",SQRMeanScoreError)
 
             
 
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import torch
 from DataTools import WaveDataReader
 from NeuralNetwork import AutoEncoder
 from NeuralNetwork import TreeModel
+from NeuralNetwork import Model
 
 Layers,AnswerSet = WaveDataReader()
 model = AutoEncoder(NumNodes=448, LatentDimensions=32)
 treemodel = TreeModel()
+OtherModel = Model(NumNodes=112)
 input_tensor = torch.randn(10, 488)
 Layers[0] = model(Layers[0].float())
 print(Layers[0].shape)
+OtherModel.ReadAndTrain(Layers,AnswerSet)
 treemodel.ReadAndTrain(Layers,AnswerSet)
 #.venv\Scripts\Activate.ps1
